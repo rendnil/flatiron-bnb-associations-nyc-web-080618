d4e4661233055afe80cd42f2329b0dcd407ae278
rendnil <andrew.lindner.15@gmail.com>
2018-08-16 15:48:43 -0400

    Done.

---
+++ db/migrate/005_create_reservations.rb
@@ -0,0 +1,14 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.datetime :checkin
+      t.datetime :checkout
+      t.integer :listing_id
+      t.integer :guest_id
+
+
+
+
+    end
+  end
+end
